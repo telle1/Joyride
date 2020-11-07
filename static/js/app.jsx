10b1012f161cc28f9ab01c778a419514a73707ac
Tiffany Luu <tiffanyell1@gmail.com>
2020-11-07 03:47:09 +0000

    React skeleton set-up

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -1,7 +1,53 @@
+const Router = ReactRouterDOM.BrowserRouter;
+const { useHistory, useParams, Redirect, Switch, Prompt, Link, Route } = ReactRouterDOM;
+
 function App(){
     return (
         <div>
-            <p>Hello</p>
+            <Router>
+                <NavBar/>
+                <Switch>
+                    <Route path="/search">
+                        <Search />
+                    </Route>
+                    <Route path="/">
+                        <HomePage />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    )
+}
+
+function NavBar(){
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                    <Link to="/">Homepage</Link>
+                    </li>
+                    <li>
+                    <Link to="/search">Search</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+function HomePage(){
+    return (
+        <div>
+            <p>homepage</p>
+        </div>
+    )
+}
+
+function Search(){
+    return (
+        <div>
+            <p>search</p>
         </div>
     )
 }
