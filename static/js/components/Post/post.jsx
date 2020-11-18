d6e8f435da52305b274d507a31a255613fd39323
Tiffany Luu <tiffanyell1@gmail.com>
2020-11-18 23:47:38 +0000

    Form validation on post ride page

--- static/js/components/Post/post.jsx
+++ static/js/components/Post/post.jsx
@@ -1,18 +1,18 @@
 const { useState, useEffect } = React 
+const {useForm} = ReactHookForm
 const {Form} = ReactBootstrap
 
 function Post({setAlertStatus, setAlertColor, alertStatus, alertColor}){
 
     const [startInput, setStartInput] = useState('')
     const [endInput, setEndInput] = useState('')
-    const [date, setDate] = useState('')
-    const [seats, setSeats] = useState(null) //this is set to null instead of 0 due to it not overwriting the placeholder
-    const [price, setPrice] = useState(null) //this is set to null instead of 0 due to the placeholder
     const [comments, setComments] = useState('')
     const [showAlert, setShowAlert] = useState('')
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const { register, errors, handleSubmit, watch } = useForm({});
+    var currentDate = new Date(); //CURRENT TIME
+
+    const onSubmit = async data => {
         fetch("/post-complete", {
             method: "POST",
             headers: {
@@ -21,20 +21,21 @@ function Post({setAlertStatus, setAlertColor, alertStatus, alertColor}){
             body: JSON.stringify({
                 from: startInput,
                 to: endInput,
-                date: date,
-                seats: seats,
-                price: price,
+                date: data.date,
+                seats: data.seats,
+                price: data.price,
                 comments: comments
             })
         })
         .then(res => res.json())
-        .then(data => {
-            setAlertStatus(data.msg)
+        .then(resp => {
+            setAlertStatus(resp.msg)
             setAlertColor('success')
             setShowAlert(true)
         })
-    }
 
+    };
+  
     return(
     <React.Fragment>
         <div className="mt-3"> {showAlert ? <UserAlert text={alertStatus} color={alertColor} setShowAlert={setShowAlert}/> 
@@ -43,7 +44,7 @@ function Post({setAlertStatus, setAlertColor, alertStatus, alertColor}){
         <Container className="top-padding">
             <Row>
             <Col xs={5}>
-                <Form onSubmit={handleSubmit} className="f-grey" method="post">
+                <Form onSubmit={e => e.preventDefault()} className="f-grey" method="post">
                     <h3 className="mb-3"> Post a Ride</h3>
                     <Form.Group as={Row} controlId="startingLocation">
                         <Form.Label column xs="3"> From </Form.Label>
@@ -60,22 +61,34 @@ function Post({setAlertStatus, setAlertColor, alertStatus, alertColor}){
                     <Form.Group as={Row} controlId="Date">
                         <Form.Label column xs="3"> Date </Form.Label>
                         <Col xs="9">
-                            <input type="date" className="form-control" name="date" id="date" 
-                            value={date} onChange={(e) => setDate(e.target.value)} required/>
+                            <input type="date" name="date" className="form-control" 
+                            ref={register({required: "Please enter a date",
+                                validate: value => new Date(value) >= currentDate || "Date cannot be in the past"
+                            })}
+                            />
+                            {errors.date && <p className="error-message mt-1 mb-n2">{errors.date.message}</p>}
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row} controlId="seats">
                         <Form.Label column xs="3"> Seats </Form.Label>
                         <Col xs="9">
-                            <input type="number" className="form-control" name="seats" placeholder="0"
-                            min="0" value={seats || ""} onChange={(e) => setSeats(e.target.value)} required/>
+                            <input type="number" name="seats" className="form-control" placeholder="0"
+                            ref={register({required: "Please enter seats available", 
+                                max: {value: 10, message: "Please enter a valid seat number"}, 
+                                min: {value: 1, message: "Not a valid seat number"}
+                            })}
+                            />
+                            {errors.seats && <p className="error-message mt-1 mb-n2">{errors.seats.message}</p>}
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row} controlId="price">
                         <Form.Label column xs="3"> Price </Form.Label>
                         <Col xs="9">
-                            <input type="number" className="form-control" name="price" placeholder="0" min="0"
-                            value={price || ""} onChange={(e) => setPrice(e.target.value)} required/>
+                            <input type="number" name="price" className="form-control" placeholder="0"
+                            ref={register({required: "Please enter a price", 
+                                min: {value: 0, message: "Not a valid price"}
+                            })}/>
+                            {errors.price && <p className="error-message mt-1 mb-n2">{errors.price.message}</p>}
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row} controlId="comments">
@@ -86,7 +99,7 @@ function Post({setAlertStatus, setAlertColor, alertStatus, alertColor}){
                         </Col>
                     </Form.Group>
                     <Row>
-                    <button type="submit" className="btn btn-theme ml-auto mr-3" id= "submit-button">Submit</button>
+                    <button type="submit" className="btn btn-theme ml-auto mr-3" onClick={handleSubmit(onSubmit)}>Submit</button>
                     </Row>
                 </Form>
             </Col>
