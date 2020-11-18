ae3223bd1f9f5bb396c3175706d53c7f4ee20b1c
Tiffany Luu <tiffanyell1@gmail.com>
2020-11-18 01:08:38 +0000

    Shifted around css

--- static/js/components/Trips/CurrentTrips/Rides/seats_modal.jsx
+++ static/js/components/Trips/CurrentTrips/Rides/seats_modal.jsx
@@ -31,11 +31,14 @@ function SeatsModal({showEdit, handleEditClose, request_id, oldSeats, seatsAvail
             <Modal.Title>UPDATE SEATS</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                    <form onSubmit={editSeats} method="post">   
-                        <p>Seats Available: {seatsAvailable} </p> 
-                        <div>
-                            <label htmlFor="seats">New Request:</label>
-                            <input type="number" className="ml-2" placeholder={oldSeats} value={seats} onChange={(e) => setSeats(e.target.value)}></input>    
+                    <form onSubmit={editSeats} className="ml-3 mr-3" method="post">   
+                        <div className="form-group row">
+                            <Col xs={4}> Seats Available: </Col>
+                            <Col>{seatsAvailable}</Col>   
+                        </div>
+                        <div className="form-group row">
+                            <Col xs={4}> <label htmlFor="seats">New Request:</label> </Col>
+                            <Col><input type="number" placeholder={oldSeats} value={seats} onChange={(e) => setSeats(e.target.value)}></input></Col>   
                         </div>  
                         <div className="form-group mb-2 mt-3">
                             <button type="submit" className="btn btn-theme form-control" onClick={handleEditClose}>Request</button> 
