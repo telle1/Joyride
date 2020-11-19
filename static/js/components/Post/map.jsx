a6101d4a8ec1d3f5d2e43259e40a20b58f1f9c60
Tiffany Luu <tiffanyell1@gmail.com>
2020-11-19 06:43:02 +0000

    Google map

---
+++ static/js/components/Post/map.jsx
@@ -0,0 +1,26 @@
+const { useEffect } = require("react");
+
+const {useRef} = React;
+
+function MyGoogleMap(){
+
+    const mapRef = useRef(null);
+    let map;
+    let marker;
+    let sf = {lat: 37.7749, lng: -122.4194}
+    let options = {
+        zoom: 5,
+        center: sf
+    }
+
+    useEffect(() => {
+        map = new window.google.maps.Map(mapRef.current, options);
+        marker = new window.google.maps.Marker({position: sf, map: map});
+    })
+
+    return (
+        <React.Fragment>
+            <div ref={mapRef} id="map" style={{width: 590, height: 540}}></div>
+        </React.Fragment>
+    )
+}
